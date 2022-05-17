231b6ded111fe6b93787d5a0296ccf14e996f8b3
nkpresh <nkwuap@gmail.com>
2022-05-17 11:16:13 +0100

    minor update

--- AtlasPacker.js
+++ AtlasPacker.js
@@ -1,19 +1,31 @@
-const { createCanvas, loadImage } = require("canvas");
-const fs = require("fs");
-const atlas = require('atlaspack');
-const { Image } = require('image-js');
+import * as PIXI from 'pixi.js'
 
-// create a canvas
-const width = 128;
-const height = 128;
-var canvas = createCanvas(width, height);
+const app = new PIXI.Application();
 
-// Create a starting atlas based on the canvas    
-    var img =new Image();
-    img.id = i;
-    img.src = '';
-    img.onload = function () {
-        atlas.pack(img);
-}
+// The application will create a canvas element for you that you
+// can then insert into the DOM.
+document.body.appendChild(app.view);
 
-console.log("empty");
+// load the texture we need
+app.loader.add('bunny', 'bunny.png').load((loader, resources) => {
+
+    // This creates a texture from a 'bunny.png' image.
+    const bunny = new PIXI.Sprite(resources.bunny.texture);
+
+    // Setup the position of the bunny
+    bunny.x = app.renderer.width / 2;
+    bunny.y = app.renderer.height / 2;
+
+    // Rotate around the center
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+
+    // Add the bunny to the scene we are building.
+    app.stage.addChild(bunny);
+
+    // Listen for frame updates
+    app.ticker.add(() => {
+        // each frame we spin the bunny around a bit
+        bunny.rotation += 0.01;
+    });
+});
