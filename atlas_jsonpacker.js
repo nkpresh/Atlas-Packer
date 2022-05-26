bcb920bd7a86604b2cebfa1616a8ac7bf341805c
nkpresh <nkwuap@gmail.com>
2022-05-26 17:56:39 +0100

    code update

--- atlas_jsonpacker.js
+++ atlas_jsonpacker.js
@@ -4,48 +4,42 @@ const lineReader = require('line-reader');
 const { delay } = require("lodash");
 
 var atlasDocuments = []
-var objectCollection = new Map();
 var fileDir = "./assets/export/goblins-pma.atlas";
 var finishedProduct = {};
 
-function loadAtlasFile() {
-    lineReader.eachLine(fileDir, function (line) {
-        atlasDocuments.push(line)
-    });
-    delay(() => {
-        breakAtlas(atlasDocuments);
-    }, 1000)
 
+
+
+async function loadAtlasFile(dir) {
+    lineReader.eachLine(dir, function (line) {
+        // atlasDocuments.push(line)
+        console.log("still reading")
+    });
+    
+    let complete = await console.log("done");
 }
 
 function breakAtlas(textFile) {
     for (var i = 0; i < textFile.length; i++) {
         var line = textFile[i].toString();
         if (line != "") {
-            // console.log(line);
             if (!line.includes(":") && line[0] != '' && !line.includes(".")) {
                 let varIndex = i;
                 varIndex += 1;
-                let stringVariables = [];
+                finishedProduct[line] = {}
                 while (varIndex < textFile.length && textFile[varIndex][0] == ' ') {
-                    // stringVariables.push(textFile[varIndex])
-                    extractVariables(textFile[varIndex], line)
-
-                    // objectCollection.set(line, variables);
+                    extractVariables(textFile[varIndex], finishedProduct[line])
                     varIndex += 1;
                 }
             }
         }
-        // finishedProduct[line] = { stringVariables };
     }
-    // console.log(line);
+    return finishedProduct
 }
 function extractVariables(stringVar, line) {
     let columnIndex = stringVar.indexOf(":");
     let key = stringVar.slice(1, columnIndex);
-    let value = stringVar.slice(columnIndex + 2, stringVar.length);
-    console.log(line);
-    finishedProduct[line] = {}
+    let value = stringVar.slice(columnIndex + 2, stringVar.length);    
     if (value == "false") {
         value = false;
     } else if (value == "true") {
@@ -53,45 +47,41 @@ function extractVariables(stringVar, line) {
     }
     else if (!Number.isNaN(Number(value))) {
         value = Number(value);
-        // console.log(value);
     }
     else if (value.includes(",")) {
         let xValue = value.slice(0, value.indexOf(","));
         let yValue = value.slice(value.indexOf(",") + 2, value.length);
         if (!Number.isNaN(Number(xValue)) && !Number.isNaN(Number(yValue))) {
-            // console.log(key)
             if (key == " xy") {
                 let position = {}
                 position.x = Number(xValue);
                 position.y = Number(yValue);
                 value = position;
-                // console.log(position+ " pos")
             } else if (key == " offset") {
                 let offset = {}
                 offset.x = Number(xValue);
                 offset.y = Number(yValue);
                 value = offset;
-                // console.log(offset)
+            } else if (key == " size") {
+                let size = {}
+                size.x = Number(xValue);
+                size.y = Number(yValue);
+                value = size;
+            } else if (key == " orig") {
+                let orig = {}
+                orig.x = Number(xValue);
+                orig.y = Number(yValue);
+                value = orig;
             }
         }
-        // console.log(key);
     }
     else {
         return new Error("this item is not compatable: " + value);
     }
-    finishedProduct[line][key] = value;
-    return finishedProduct;
+    line[key] = value;
+    return line;
 }
-
-function nodeTest() {
-    let arrayObj = ["0", "Like", "learn", "7,7", "Look", "9", "Life", "Lenghtfully", "19", "Lie", "Low", "Lately"];
-    const dataArr = arrayObj.reduce((acc, cur, index) => ({ ...acc, [String.fromCharCode(97 + index)]: cur }), Object.create({}))
-    let testFunc = testReturn();
-    console.log(testFunc);
-}
-loadAtlasFile();
-// nodeTest();
-
-function testReturn() {
-    return "working";
-}
+loadAtlasFile(fileDir);
+    // breakAtlas(atlasDocuments);
+    // console.log("finished reading");
+// process_atlas(fileDir);
